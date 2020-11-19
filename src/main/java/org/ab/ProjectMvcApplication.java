3e0e0d749deae9b348e4e2a4851033859ec00d75
mayibee <ale_bravo_20@yahoo.com>
2020-11-18 21:33:35 -0500

    Initial commit

---
+++ src/main/java/org/ab/ProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package org.ab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ab/ProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

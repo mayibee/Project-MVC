9cd569a458b62f3d7078e7529eb415ff4343f2df
mayibee <ale_bravo_20@yahoo.com>
2020-11-21 13:37:26 -0500

    fixing user

---
+++ src/main/java/org/ab/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.ab.controller;
+
+import org.ab.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/create")
+    public String userCreate(Model model){
+        model.addAttribute("user", new UserDTO());
+        return "user/create";
+    }
+}

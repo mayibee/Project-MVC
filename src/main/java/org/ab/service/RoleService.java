f594b7c838c71bfaa79dd6147cc4bfcecd937b3d
mayibee <ale_bravo_20@yahoo.com>
2020-11-21 14:44:40 -0500

    services and implementation

---
+++ src/main/java/org/ab/service/RoleService.java
@@ -0,0 +1,9 @@
+package org.ab.service;
+
+import org.ab.dto.RoleDTO;
+import org.ab.dto.UserDTO;
+
+import java.util.List;
+
+public interface RoleService extends CrudService<RoleDTO, Long>{
+}
---
+++ src/main/java/org/ab/service/UserService.java
@@ -0,0 +1,20 @@
+package org.ab.service;
+
+import org.ab.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService extends CrudService<UserDTO, String>{
+    //Dont need the methods anymore as they all come from CrudService
+//    //save
+//    UserDTO save(UserDTO user);
+//    //search user by username
+//    UserDTO findById(Long id);
+//    UserDTO findByUsername(String username);
+//    //retrieve all users in list
+//    List<UserDTO> findAll();
+//    //delete
+//    void delete(UserDTO user);
+//    void deleteByUsername(String username);
+//    void deleteById(Long id);
+}

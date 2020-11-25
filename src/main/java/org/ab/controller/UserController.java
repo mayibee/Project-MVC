b2bb6bb4415b767a0b5cbcadde0e1bacfa0a2411
mayibee <ale_bravo_20@yahoo.com>
2020-11-24 19:18:29 -0500

    post create user

--- src/main/java/org/ab/controller/UserController.java
+++ src/main/java/org/ab/controller/UserController.java
@@ -1,17 +1,43 @@
 package org.ab.controller;
 
 import org.ab.dto.UserDTO;
+import org.ab.model.User;
+import org.ab.service.RoleService;
+import org.ab.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
+
     @GetMapping("/create")
     public String userCreate(Model model){
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+        return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model){
+        userService.save(user);
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
         return "user/create";
     }
 }
